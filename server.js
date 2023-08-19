59677f9fb24950731d54b4e6dc28d2d6d3b630f4
Yuna Pahk <new1@new1s-MacBook-Pro.local>
2023-08-19 12:49:47 -0700

    Starting with just the dog model

--- server.js
+++ server.js
@@ -1,6 +1,3 @@
-///////////////////////////////
-// DEPENDENCIES
-///////////////////////////////
 require('dotenv').config();
 const { PORT = 4567, DATABASE_URL } = process.env;
 const express = require('express');
@@ -13,9 +10,6 @@ app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
 
-///////////////////////////////
-// DATABASE CONNECTION
-///////////////////////////////
 mongoose.connect(DATABASE_URL, {
     useUnifiedTopology: true,
     useNewUrlParser: true,
@@ -30,55 +24,134 @@ mongoose.connection
     .on('close', () => console.log('Disconnected from mongoose... Fix me!'))
     .on('error', (error) => console.log(error));
 
-///////////////////////////////
-// MODELS 
-///////////////////////////////
-const Dog = require('./models/Dog');
-const Match = require('./models/Match');
-const DogPark = require('./models/DogPark');
-const Message = require('./models/Message');
-const Review = require('./models/Review');
-const User = require('./models/User');
-const Location = require('./models/Location');
+// DogSchema definition
+const DogSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    breed: String,
+    age: Number,
+    // An array of image URLs to represent the dog.
+    // The user must provide at least 3 images.
+    images: {
+        type: [String], 
+        validate: {
+            validator: function(v) {
+                return v.length >= 3;
+            },
+            message: '{PATH} should have at least 3 images.'
+        },
+        required: [true, 'Dog must have at least 3 images.']
+    },
+    size: { type: String, required: true },
+    activityLevel: { type: String, required: true },
+});
 
-///////////////////////////////
-// ROUTES
-///////////////////////////////
-const dogRoutes = require('./routes/dogs');
-const matchRoutes = require('./routes/matches');
-const dogParkRoutes = require('./routes/dogParks');
-const messageRoutes = require('./routes/messages');
-const reviewRoutes = require('./routes/reviews');
-const userRoutes = require('./routes/users');
-const locationRoutes = require('./routes/locations');
+// Matching function as a method
+DogSchema.methods.getMatchScoreWith = function(otherDog) {
+    let points = 100;
 
-app.use('/dogs', dogRoutes);
-app.use('/matches', matchRoutes);
-app.use('/dogparks', dogParkRoutes);
-app.use('/messages', messageRoutes);
-app.use('/reviews', reviewRoutes);
-app.use('/users', userRoutes);
-app.use('/locations', locationRoutes);
+    if (this.breed === otherDog.breed) {
+        points += 10;
+    } else {
+        points -= 10;
+    }
+
+    if (this.size === otherDog.size) {
+        points += 5;
+    } else {
+        points -= 5;
+    }
+
+    if (this.activityLevel === otherDog.activityLevel) {
+        points += 5;
+    } else {
+        points -= 5;
+    }
+
+    if (Math.abs(this.age - otherDog.age) < 2) {
+        points += 3;
+    } else {
+        points -= 3;
+    }
+
+    return Math.max(points, 0);
+};
+
+const Dog = mongoose.model('Dog', DogSchema);
+
+// Routes for Dog
+const router = express.Router();
+
+// INDEX - GET - /dogs - gets all dogs
+router.get("/", async (req, res) => {
+    try {
+        res.json(await Dog.find({}));
+    } catch (error) {
+        res.status(400).json({ message: "Error fetching dogs.", error });
+    }
+});
+
+// CREATE - POST - /dogs - create a new dog
+router.post("/", async (req, res) => {
+    try {
+        res.json(await Dog.create(req.body));
+    } catch (error) {
+        res.status(400).json({ message: "Error creating dog.", error });
+    }
+});
+
+// SHOW - GET - /dogs/:id - get a single dog
+router.get("/:id", async (req, res) => {
+    try {
+        const dog = await Dog.findById(req.params.id);
+        if (!dog) {
+            return res.status(404).json({ message: "Dog not found." });
+        }
+        res.json(dog);
+    } catch (error) {
+        res.status(400).json({ message: "Error fetching the dog.", error });
+    }
+});
+
+// UPDATE - PUT - /dogs/:id - update a single dog
+router.put("/:id", async (req, res) => {
+    try {
+        const updatedDog = await Dog.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        if (!updatedDog) {
+            return res.status(404).json({ message: "Dog not found." });
+        }
+        res.json(updatedDog);
+    } catch (error) {
+        res.status(400).json({ message: "Error updating the dog.", error });
+    }
+});
+
+// DELETE - DELETE - /dogs/:id - delete a single dog
+router.delete("/:id", async (req, res) => {
+    try {
+        const deletedDog = await Dog.findByIdAndRemove(req.params.id);
+        if (!deletedDog) {
+            return res.status(404).json({ message: "Dog not found." });
+        }
+        res.json(deletedDog);
+    } catch (error) {
+        res.status(400).json({ message: "Error deleting the dog.", error });
+    }
+});
+
+app.use('/dogs', router);
 
 // Test route
 app.get('/', (req, res) => {
     res.send('Hi server');
 });
 
-///////////////////////////////
-// ADDITIONAL ERROR HANDLING
-///////////////////////////////
-// 404 - Not Found Middleware
+// Additional error handling
 app.use((req, res, next) => {
     res.status(404).send('Route not found');
 });
 
-// General Error Handling Middleware
 app.use((error, req, res, next) => {
     res.status(500).json({ error: error.message });
 });
 
-///////////////////////////////
-// LISTENER
-///////////////////////////////
 app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
