4ccc5691c9f00f7a08d291f982ae451e2a009adf
Yuna Pahk <new1@new1s-MacBook-Pro.local>
2023-08-22 19:17:25 -0700

    testing

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const morgan = require('morgan');
 const app = express();
+const cookieParser = require('cookie-parser');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 //////////////////////
 //DATABASE CONNECTION
@@ -25,13 +28,21 @@ mongoose.connection
 ///////////////////////////////
 // MODELS
 ////////////////////////////////
+const UserSchema = new mongoose.Schema({
+    username: { type: String, required: true, unique: true },
+    password: {type: String, required: true},
+  });
+  
+const User = mongoose.model("User", UserSchema);
+
 const DogSchema = new mongoose.Schema({
     name: { type: String, required: true },
     breed: { type: String, required: true },
     age: { type: Number, required: true },
     image: String,
     size:  { type: String, required: true },
-    activityLevel: { type: String, required: true }
+    activityLevel: { type: String, required: true },
+    username: String
   });
   
   const  Dog = mongoose.model("Dog", DogSchema);
@@ -39,10 +50,35 @@ const DogSchema = new mongoose.Schema({
 ///////////////////////////////
 // MiddleWare
 ////////////////////////////////
-app.use(cors());
+app.use(
+    cors({
+      origin: "http://localhost:3000",
+      credentials: true,
+    })
+  );
 app.use(morgan('dev'));
 app.use(express.json());
-  
+app.use(cookieParser());
+app.get('/cooketest', (req, res) => {
+    res.json(req.cookies);
+});
+app.get("/logout", (req, res) => {
+    res.clearCookie("token");
+    res.json({ message: "You have been logged out" });
+});
+
+////////////////////////////////
+// Custom Auth Middleware Function
+////////////////////////////////
+async function authCheck(req, res, next){
+    if(req.cookies.token){
+      const payload = jwt.verify(req.cookies.token, process.env.SECRET)
+      req.payload = payload;
+      next();
+    } else {
+      res.status(400).json({ error: "You are not authorized" });
+    }
+  }
 
 //////////////////////
 //ROUTES
@@ -53,63 +89,112 @@ app.get('/', (req, res) => {
     res.json({hello: "server"});
 });
 
+// AUTH ROUTES
+// SIGNUP ROUTE
+app.post("/signup", async (req, res) => {
+    try {
+        const { username, password } = req.body;
+        const hashedPassword = await bcrypt.hash(password, await bcrypt.genSalt(10));
+        const user = await User.create({ username, password: hashedPassword });
+        res.json(user);
+    } catch (error) {
+        console.error(error); 
+        res.status(400).json({ error: "Failed to create user" });
+    }
+});
+
+// LOGIN ROUTE
+app.post("/login", async (req, res) => {
+    try {
+      const { username, password } = req.body;
+      const user = await User.findOne({ username });
+      console.log(user)
+      if (!user) {
+        throw new Error("No user with that username found");
+      }
+      const passwordCheck = await bcrypt.compare(password, user.password);
+      if (!passwordCheck) {
+        throw new Error("Password does not match");
+      }
+      const token = jwt.sign({ username: user.username }, process.env.SECRET);
+      res.cookie("token", token, {
+        // can only be accessed by server requests
+        httpOnly: true,
+        // path = where the cookie is valid
+        path: "/",
+        // domain = what domain the cookie is valid on
+        domain: "localhost",
+        // secure = only send cookie over https
+        secure: false,
+        // sameSite = only send cookie if the request is coming from the same origin
+        sameSite: "lax", // "strict" | "lax" | "none" (secure must be true)
+        // maxAge = how long the cookie is valid for in milliseconds
+        maxAge: 3600000, // 1 hour
+      });
+      res.json(user);
+    } catch (error) {
+      res.status(400).json({ error: error.message });
+    }
+  });
+
 // INDEX - GET - /dogs - gets all dogs
-app.get("/dogs", async (req, res) => {
+app.get("/dogs", authCheck, async (req, res) => {
     try {
-        res.json(await Dog.find({}));
+      // fetch all dogs from the database
+      const dogs = await Dog.find({});
+      // send JSON of all dogs
+      res.json(dogs);
     } catch (error) {
-        res.status(400).json({ message: "Error fetching dogs.", error });
+      // send error as JSON
+      res.status(400).json({ error });
     }
-});
-
+  });
+  
 // CREATE - POST - /dogs - create a new dog
-app.post("/dogs", async (req, res) => {
+app.post("/dogs", authCheck, async (req, res) => {
     try {
-        res.json(await Dog.create(req.body));
+      const dog = await Dog.create(req.body);
+      personalbar.username = req.payload.username;
+      res.json(dog);
     } catch (error) {
-        res.status(400).json({ message: "Error creating dog.", error });
+      res.status(400).json({ error });
     }
-});
-
-
-// SHOW - GET - /dogs/:id - get a single dog
-app.get("/dogs/:id", async (req, res) => {
+  });
+  
+  // SHOW - GET - /dogs/:id - get a single dog
+  app.get("/dogs/:id", authCheck, async (req, res) => {
     try {
-        const dog = await Dog.findById(req.params.id);
-        if (!dog) {
-            return res.status(404).json({ message: "Dog not found." });
-        }
-        res.json(dog);
+      const dog = await Dog.findById(req.params.id);
+      res.json(dog);
     } catch (error) {
-        res.status(400).json({ message: "Error fetching the dog.", error });
+      res.status(400).json({ error });
     }
-});
-
-// UPDATE - PUT - /dogs/:id - update a single dog
-app.put("/dogs/:id", async (req, res) => {
+  });
+  
+  // UPDATE - PUT - /dogs/:id - update a single dog
+  app.put("/dogs/:id", authCheck, async (req, res) => {
     try {
-        const updatedDog = await Dog.findByIdAndUpdate(req.params.id, req.body, { new: true });
-        if (!updatedDog) {
-            return res.status(404).json({ message: "Dog not found." });
-        }
-        res.json(updatedDog);
+      const dog = await 
+      Dog.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+      });
+      res.json(dog);
     } catch (error) {
-        res.status(400).json({ message: "Error updating the dog.", error });
+      res.status(400).json({ error });
     }
-});
-
-// DELETE - DELETE - /dogs/:id - delete a single dog
-app.delete("/dogs/:id", async (req, res) => {
+  });
+  
+  // DELETE - DELETE - /dogs/:id - delete a single dog
+  app.delete("/dogs/:id", authCheck, async (req, res) => {
     try {
-        const deletedDog = await Dog.findByIdAndRemove(req.params.id);
-        if (!deletedDog) {
-            return res.status(404).json({ message: "Dog not found." });
-        }
-        res.json(deletedDog);
+      const dog = await 
+      Dog.findByIdAndDelete(req.params.id);
+      res.status(204).json(dog);
     } catch (error) {
-        res.status(400).json({ message: "Error deleting the dog.", error });
+      res.status(400).json({ error });
     }
-});
+  });
+  
 
 
 //////////////////////
